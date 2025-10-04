fbaadb6a3d37de605dc5d93e02b91018176d640e
DerekDao01 <dddao1@uci.edu>
2025-10-04 15:43:32 -0700

    fetch user data from session on user page

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,28 +1,64 @@
 import { useEffect, useState } from 'react';
 import { supabase} from '@/utils/supabase';
 import { Text, View } from 'react-native';
+import type { Session } from '@supabase/supabase-js';
 
 interface User {
   first_name: string;
   last_name: string;
+  avatar_url?: string;
 }
 
 export default function User() {
-  const [users, setUsers] = useState<User[]>([]);
+  const [session, setSession] = useState<Session | undefined | null>()
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    const fetchUsers = async() => {
-        const { data, error } = await supabase
-          .from('users')
-          .select();
-        console.log(data);
+
+    const fetchSession = async () => {
+      setIsLoading(true)
+
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession()
+
+      if (error) {
+        console.error('Error fetching session:', error)
+      }
+
+      setSession(session)
+      setIsLoading(false)
     }
 
-    // fetchUsers();
-  }, [])
+    fetchSession()
+  }, []);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (!session?.user) return;
+
+      const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('user_id', session.user.id)
+        .single();
+
+      if (error) {
+        console.error('Error fetching user:', error);
+      } else {
+        setUser(data);
+      }
+    };
+
+    fetchUser();
+    console.log("User fetched: ", user);
+  }, [session]);
+
   return (
     <View>
-      <Text className="color: blue;">a</Text>
+      <Text style={{ color: 'blue' }}>a</Text>
     </View>
   );
 }
