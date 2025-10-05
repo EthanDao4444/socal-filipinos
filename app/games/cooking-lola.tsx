0100cb6b21b14af6b466ba9d3c02a0276d7ac051
iangalvez <iangalvez@g.ucla.edu>
2025-10-05 01:42:02 -0700

    primitive ahh lumpia making

--- app/games/cooking-lola.tsx
+++ app/games/cooking-lola.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, Image } from 'react-native';
 import { Link, Stack } from 'expo-router';
 
 import React from 'react';
 import Animated, {
+  withTiming,
   useSharedValue,
   useAnimatedStyle,
 } from 'react-native-reanimated';
 import {
+  Directions,
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
@@ -15,8 +17,80 @@ import {
 import { StyleSheet, Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('screen');
+const images = [
+  require('../../assets/cooking-lola/lumpia-1.jpg'),
+  require('../../assets/cooking-lola/lumpia-2.jpg'),
+  require('../../assets/cooking-lola/lumpia-3.jpg'),
+  require('../../assets/cooking-lola/lumpia-3b.png'),
+  require('../../assets/cooking-lola/lumpia-4.jpg'),
+  require('../../assets/cooking-lola/lumpia-5.jpg'),
+  require('../../assets/cooking-lola/lumpia-6.jpg'),
+];
 
 export default function CookingLola() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const goToPreviousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  const tap = Gesture.Tap()
+    .maxDuration(250)
+    .onEnd(() => {
+      console.log('Tap!');
+    });
+
+  const swipeRight = Gesture.Fling()
+    .direction(Directions.RIGHT)
+    .onEnd((event) => {
+      console.log('Right swipe!');
+      if (currentImageIndex === 3) {
+        goToNextImage();
+      }
+    })
+    .runOnJS(true);
+
+  const swipeLeft = Gesture.Fling()
+    .direction(Directions.LEFT)
+    .onEnd((event) => {
+      console.log('Left swipe!');
+      if (currentImageIndex === 2) {
+        goToNextImage();
+      }
+    })
+    .runOnJS(true);
+
+  const swipeUp = Gesture.Fling()
+    .direction(Directions.UP)
+    .onEnd((event) => {
+      console.log('Up swipe!');
+      if (currentImageIndex !== 2 && currentImageIndex !== 3) {
+        goToNextImage();
+      }
+    })
+    .runOnJS(true);
+
+  return (
+    <GestureHandlerRootView className="flex-1 items-center justify-center">
+      <GestureDetector gesture={Gesture.Exclusive(swipeRight, swipeLeft, swipeUp, tap)}>
+        <Animated.View className="h-[300px] w-[400px]">
+          <Image
+            source={images[currentImageIndex]}
+            className="flex-1 object-cover h-[300px] w-[400px]"/>
+        </Animated.View>
+      </GestureDetector>
+    </GestureHandlerRootView>
+  );
+}
+
+
+/* export default function CookingLola() {
   const translationX = useSharedValue(0);
   const translationY = useSharedValue(0);
   const prevTranslationX = useSharedValue(0);
@@ -62,18 +136,5 @@ export default function CookingLola() {
       </GestureDetector>
     </GestureHandlerRootView>
   );
-}
+} */
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: '#b58df1',
-    borderRadius: 20,
-  },
-});
