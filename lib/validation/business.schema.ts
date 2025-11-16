4fcea9f709a98b95457530895c75d8cf1747372a
EthanDao4444 <ethandao4444@gmail.com>
2025-11-16 15:20:15 -0800

    feat: Added Business schema validator

--- lib/validation/business.schema.ts
+++ lib/validation/business.schema.ts
@@ -1,1 +1,12 @@
 // TODO: create business schema validation (for uploading and fetching)
+import * as z from "zod";
+
+const Business = z.object({
+  business_id: z.uuid(),
+  business_name: z.string(),
+  business_type: z.string().optional(),
+  location: z.string(), // we are currently using PostGIS Geography type, need to check if this is valid with that implementation
+  location_address: z.string(),
+  image_url: z.url(),
+  created_at: z.iso.datetime(),
+})
